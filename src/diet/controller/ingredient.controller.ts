ba025e3476832da463f4f0849fbe34fee279d393
Sergei Krivosheenko <serkyron@gmail.com>
2022-11-30 18:08:06 +0100

    guard ingredient route

--- src/diet/controller/ingredient.controller.ts
+++ src/diet/controller/ingredient.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { DietService } from '../service/diet.service';
 import { Ingredient } from "../entity/ingredient.entity";
 import { ResponseDto } from "../dto/response.dto";
 import { IngredientService } from "../service/ingredient.service";
+import { JwtAuthGuard } from "../../auth/guards/jwt-auth.guard";
 
 @Controller()
 export class IngredientController {
@@ -10,6 +11,7 @@ export class IngredientController {
         private readonly ingredientService: IngredientService
     ) {}
 
+    @UseGuards(JwtAuthGuard)
     @Get('ingredient')
     public async list(): Promise<ResponseDto<Ingredient>> {
         const ingredients = await this.ingredientService.getAll();
