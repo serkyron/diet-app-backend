660071d2ba102a2d10ce85028ea8f3ac6a9a2b11
Sergei Krivosheenko <serkyron@gmail.com>
2023-03-23 02:26:04 +0100

    make day meals optional

--- src/diet/dto/day.dto.ts
+++ src/diet/dto/day.dto.ts
@@ -5,23 +5,13 @@ export class DayDto {
     @IsString()
     readonly name: string;
 
-    @IsNotEmpty()
-    @IsObject()
     readonly breakfast: {id: number};
 
-    @IsNotEmpty()
-    @IsObject()
     readonly snack1: {id: number};
 
-    @IsNotEmpty()
-    @IsObject()
     readonly lunch: {id: number};
 
-    @IsNotEmpty()
-    @IsObject()
     readonly snack2: {id: number};
 
-    @IsNotEmpty()
-    @IsObject()
     readonly dinner: {id: number};
 }
